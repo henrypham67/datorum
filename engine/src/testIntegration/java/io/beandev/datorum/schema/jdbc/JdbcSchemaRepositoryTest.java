b925d6883bb5fc9f618a2f66d988d4676187400d
PeterBean <peterbean410@gmail.com>
2024-07-23 22:31:00 +0700

    Add some test folders

---
+++ engine/src/testIntegration/java/io/beandev/datorum/schema/jdbc/JdbcSchemaRepositoryTest.java
@@ -0,0 +1,13 @@
+package io.beandev.datorum.schema.jdbc;
+
+import org.junit.jupiter.api.Test;
+
+import static java.lang.System.out;
+
+public class JdbcSchemaRepositoryTest {
+    @Test
+    void myTest() {
+        out.print("Hello");
+        out.println("testing ");
+    }
+}
